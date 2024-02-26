d49a5319587818977ff2702c99e084c21b7ca2c6
Anpanprang <64070030@it.kmitl.ac.th>
2024-02-26 14:40:40 +0700

    Update Navbar.vue

--- EduPulse/src/components/Navbar.vue
+++ EduPulse/src/components/Navbar.vue
@@ -33,7 +33,7 @@
                 :class="[
                   item.current
                     ? 'bg-gray-900 text-white'
-                    : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    : 'text-gray-500 hover:bg-gray-700 hover:text-white',
                   'rounded-md px-3 py-2 text-sm font-medium',
                 ]"
                 :aria-current="item.current ? 'page' : undefined"
@@ -121,7 +121,7 @@
           :class="[
             item.current
               ? 'bg-gray-900 text-white'
-              : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+              : 'text-gray-700 hover:bg-gray-700 hover:text-white',
             'block rounded-md px-3 py-2 text-base font-medium',
           ]"
           :aria-current="item.current ? 'page' : undefined"
